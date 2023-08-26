644f6c43e9f56e0c0737e3845793bdc9cec2ec0d
Huey-dev <matthewokadinya@gmail.com>
2023-08-26 21:02:04 +0100

    hhd

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite +y React</h1>
       <div className="card">
         <button onClick={() => setCount((prevCount) => prevCount > 0 ? prevCount - 1 : 0 )}>
           - 
