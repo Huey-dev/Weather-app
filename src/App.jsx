899876d39609299ea805be74e1fa035baf22e66c
Huey-dev <matthewokadinya@gmail.com>
2023-08-28 13:08:50 +0100

    initialize project

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite +y React</h1>
-      <div className="card">
-        <button onClick={() => setCount((prevCount) => prevCount > 0 ? prevCount - 1 : 0 )}>
-          - 
-        </button>
-        <span className='count'>{count}</span>
-        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-          +
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>Weather-App</div>
   )
 }
 
