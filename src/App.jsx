d238f9c2186a49095d48f8f43e767456e16be950
Huey-dev <matthewokadinya@gmail.com>
2023-09-06 23:42:44 +0100

    removed dotenv, corrected api masking

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import CurrentWeather from "./components/CurrentWeather";
 import WeatherForecast from "./components/WeatherForecast";
-import dotenv from 'dotenv';
-dotenv.config();
+
 const App = () => {
   const handleonSearchChange = (searchData) => {
     console.log(searchData);
